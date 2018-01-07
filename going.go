483637e08ba8406bb753bfb26009645aab5cc939
ahmed fadhil <fadhil@fbk.eu>
2018-01-07 12:53:38 +0100

    retrieve data from site

--- going.go
+++ going.go
@@ -1,30 +1,65 @@
+//start with web7 / edit7, then we bring in combined2.go.
+
 package main
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
 	"html/template"
+	"io/ioutil"
+	"encoding/xml"
 )
+
+type Sitemapindex struct {
+	Locations []string `xml:"sitemap>loc"`
+
+}
+
+type News struct {
+	Titles []string `xml:"url>news>title"`
+    Keywords []string `xml:"url>news>keywords"`
+    Locations []string `xml:"url>news>loc"`
+
+}
+
+type NewsMap struct {
+	Keywords string
+	Location string
+}
 
 type NewsAggPage struct {
 	Title string
-	News  string
+	News  map[string]NewsMap
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Whoa, Go is neat!</h1>")
 }
 
 func newsAggHandler(w http.ResponseWriter, r *http.Request) {
-	p := NewsAggPage{Title: "Amazing Title", News: "Some news"}
-	t, _:= template.ParseFiles("basic.html")
-	//fmt.Println(err)
-	fmt.Println(t.Execute(w, p))
+	var s Sitemapindex
+	var n News
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes, &s)
+	news_map := make(map[string]NewsMap)
+	for _, Location := range s.Locations {
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		xml.Unmarshal(bytes, &n)
+		for idx, _ := range n.Keywords {
+			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		}
 
-}
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1> Whoa, Go is neat!</h1>")
+	}
 
+	p := NewsAggPage{Title: "Amazing News Aggregator", News: news_map}
+	t, _ := template.ParseFiles("basic.html")
+	t.Execute(w, p)
 }
+
 func main() {
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/agg/", newsAggHandler)
 	http.ListenAndServe(":8000", nil)
-
 }
